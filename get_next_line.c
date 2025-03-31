d2985ab4a5ef80f0e657be90d0bb2e9718ce88bb
stempels <stempels@student.s19.be>
2025-04-01 01:11:07 +0200

    to test

--- get_next_line.c
+++ get_next_line.c
@@ -1,88 +1,90 @@
+
+static char	*gnl_read(int fd, char *inreading, int lenght);
+static char	*if_new_line(char *inreading, char *src, int length, int n);
+static char	*gnl_cpy(char *dst, char *src, int start, int end);
+static char	*ft_free(int fd, char **to_free);
+
 char	*get_next_line(int	fd)
 {
 	int			i;
 	char		*new_line;
-	static char	*inreading[OPEN_MAX + 1];
+	char		*new_stash;
+	static char	*stash[OPEN_MAX + 1];
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0 || read(fd, 0, 0) <= 0)
-		return (ft_free(inreading[fd]));
-	if (!inreading)
-		inreading[fd] = NULL;
-/*	i = 0;
-	if (inreading[fd])
+		return (ft_free(fd, stash));
+	i = 0;
+	if (stash[fd])
 	{
-		while (inreading[fd][i])
+		while (stash[fd][i])
 		{
-			if (inreading[fd][i] == "\n")
-			{
-				new_line = if_new_line(inreading[fd], inreading[fd], 0, i);
-				if (!new_line)
-					return (ft_free(inreading[fd]));
-				if (i < ft_strlen(inreading[fd]))
+			if (stash[fd][i] == "\n")
+			{	new_line = if_new_line(stash[fd], stash[fd], 0, i);
+				if (i < ft_strlen(stash[fd]))
 				{
-					inreading[fd] = if_new_line(inreading[fd], &inreading[fd][i], 0, ft_strlen(inreading[fd]) - i);
-					if (!inreading[fd])
-						return (ft_free(new_line);
+					stash = if_new_line(stash, &stash[i + 1], 0, ft_strlen(stash[fd]));
+					if (!stash)
+						return (NULL);
 				}
 				return (new_line);
 			}
 			i++;
 		}
-	}*/
-	new_line = gnl_read(fd, inreading[fd], 0);
-/*	if (inreading[fd] && new_line)
-		new_line = gnl_cpy(new_line, inreading[fd], 0, i);*/
+		new_stash = stash[fd];
+		stash[fd] = NULL;
+		new_line = gnl_read(fd, stash[fd], i);
+		new_line = gnl_cpy(new_line, new_stash, 0, i);
+		free(new_stash);
+	}
+	else
+		new_line = gnl_read(fd, stash[fd], i);
 	return (new_line);
 }
 
-char	*gnl_read(int fd, char *inreading, int lenght)
+static char	*gnl_read(int fd, char *inreading, int lenght)
 {
 	int		i;
 	int		byte_read;
 	char	*new_line;
 	char	buffer[BUFFER_SIZE];
 
-	if (lenght == 0 && inreading)
-	{
-		buffer = inreading;
-		byte_read = ft_strlen(inreading);
-	}
-	else
-		byte_read = read(fd, buffer, BUFFER_SIZE);
-	if (lenght != 0 && byte_read < 0)
-		return (ft_free(inreading));
+	byte_read = read(fd, buffer, BUFFER_SIZE);
+	if (lenght == 0 && byte_read < 0)
+		return (ft_free(fd, &inreading));
 	i = 0;
 	while (i < byte_read)
 	{
 		if (buffer[i] == "\n")
 		{
-			new_line = if_new_line(new_line, buffer, lenght, lenght + i);
+			new_line = if_new_line(inreading, buffer, length, i);
 			if (!new_line)
-				return (ft_free(inreading));
-			if (i != byte_read - 1)
+				return (ft_free(fd, &inreading));
+			if (i < byte_read - 1)
 			{
-				inreading = if_new_line(inreading, &buffer[i], 0, byte_read - i);
+				inreading = if_new_line(inreading, &buffer[i + 1], 0, byte_read - i);
 				if (!inreading)
-					return (ft_free(new_line));
+					return (ft_free(fd, &new_line));
 			}
-			return (new_line);
 		}
 		i++;
 	}
-	new_line = gnl_read(fd, inreading, lenght + byte_read);
-	return (gnl_cpy(new_line, buffer, lenght, byte_read));
+	new_line = gnl_read(fd, inreading, length + byte_read);
+	return (gnl_cpy(new_line, buffer, length, byte_read));
 }
 
-char	*if_new_line(char *dst, char *src, int start, int end)
+static char	*if_new_line(char *inreading, char *src, int length, int n)
 {
-	dst = (char *) malloc(sizeof(char) * (end + 1));
-	if (!dst)
+	char	*new_line;
+
+	new_line = (char *) malloc(sizeof(char) * (length + n + 1));
+	if (!new_line)
 		return (NULL);
-	dst[end + 1] = "\0";
-	dst = gnl_cpy(dst, src, start, end);
-	return (dst);
+	new_line[length + n + 1] = '\0';
+	new_line = gnl_cpy(new_line, src, length, n);
+	return (new_line);
 }
-char	*gnl_cpy(char *dst, char *src, int start, int end)
+
+static char	*gnl_cpy(char *dst, char *src, int start, int end)
 {
 	int	i;
 
@@ -97,10 +99,12 @@ char	*gnl_cpy(char *dst, char *src, int start, int end)
 	return (dst);
 }
 
-char	*ft_free(char *to_free)
+static char	*ft_free(int fd, char **to_free)
 {
-	if (to_free)
-		free(to_free);
-	to_free = NULL;
-	return (to_free);
+	if (fd < 0)
+		fd = -1 * fd;
+	if (to_free[fd])
+		free(to_free[fd]);
+	to_free[fd] = NULL;
+	return (to_free[fd]);
 }
