b84fbb2060526f29890cd507e621b50157cabd46
stempels <stempels@student.s19.be>
2025-04-01 10:24:47 +0200

    alternative

--- get_next_line.c
+++ get_next_line.c
@@ -14,34 +14,31 @@ char	*get_next_line(int	fd)
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0 || read(fd, 0, 0) <= 0)
 		return (ft_free(fd, stash));
 	i = 0;
-	if (stash[fd])
+	if (!stash[fd])
+		return (gnl_read(fd, stash[fd], i));
+	while (stash[fd][i])
 	{
-		while (stash[fd][i])
-		{
-			if (stash[fd][i] == "\n")
-			{	new_line = if_new_line(stash[fd], stash[fd], 0, i);
-				if (i < ft_strlen(stash[fd]))
-				{
-					stash = if_new_line(stash, &stash[i + 1], 0, ft_strlen(stash[fd]));
-					if (!stash)
-						return (NULL);
-				}
-				return (new_line);
-			}
-			i++;
+		if (stash[fd][i] == "\n")
+			new_line = (char *) malloc(sizeof(char) * (i + 1));
+			if (!new_line)
+				return (ft_free(fd, &stash));
+			new_line[i + 1] = '\0';
+			stash[fd] = if_new_line(&new_line[i], stash[fd], i, ft_strlen(stash[fd]));
+			if (i < ft_strlen(stash[fd]) - 1 && !stash)
+				return (free(new_line);
+			return (new_line);
 		}
-		new_stash = stash[fd];
-		stash[fd] = NULL;
-		new_line = gnl_read(fd, stash[fd], i);
-		new_line = gnl_cpy(new_line, new_stash, 0, i);
-		free(new_stash);
+		i++;
 	}
-	else
-		new_line = gnl_read(fd, stash[fd], i);
+	new_stash = stash[fd];
+	stash[fd] = NULL;
+	new_line = gnl_read(fd, stash[fd], i);
+	new_line = gnl_cpy(new_line, new_stash, 0, i);
+	free(new_stash);
 	return (new_line);
 }
 
-static char	*gnl_read(int fd, char *inreading, int lenght)
+static char	*gnl_read(int fd, char *stash, int lenght)
 {
 	int		i;
 	int		byte_read;
@@ -50,41 +47,45 @@ static char	*gnl_read(int fd, char *inreading, int lenght)
 
 	byte_read = read(fd, buffer, BUFFER_SIZE);
 	if (lenght == 0 && byte_read < 0)
-		return (ft_free(fd, &inreading));
+		return (NULL);
 	i = 0;
 	while (i < byte_read)
 	{
 		if (buffer[i] == "\n")
 		{
-			new_line = if_new_line(inreading, buffer, length, i);
+			new_line = (char *) malloc(sizeof(char) * (length + n + 1));
 			if (!new_line)
-				return (ft_free(fd, &inreading));
-			if (i < byte_read - 1)
-			{
-				inreading = if_new_line(inreading, &buffer[i + 1], 0, byte_read - i);
-				if (!inreading)
-					return (ft_free(fd, &new_line));
-			}
+				return (NULL);
+			new_line[length + n + 1] = '\0';
+			stash = if_new_line(&new_line[length], buffer, i, byte_read);
+			if (i < byte_read - 1 && !stash)
+				return (free(new_line);
+			return (new_line);
 		}
 		i++;
 	}
 	new_line = gnl_read(fd, inreading, length + byte_read);
-	return (gnl_cpy(new_line, buffer, length, byte_read));
-}
-
-static char	*if_new_line(char *inreading, char *src, int length, int n)
-{
-	char	*new_line;
-
-	new_line = (char *) malloc(sizeof(char) * (length + n + 1));
-	if (!new_line)
-		return (NULL);
-	new_line[length + n + 1] = '\0';
-	new_line = gnl_cpy(new_line, src, length, n);
+	gnl_cpy(&new_line[length], buffer, byte_read);
 	return (new_line);
 }
 
-static char	*gnl_cpy(char *dst, char *src, int start, int end)
+static char	*if_new_line(char *new_line, char *src, int n, int length)
+{
+	char	*stash;
+
+	gnl_cpy(new_line, src, n);
+	if (n < length - 1)
+	{
+		stash = (char *) malloc(sizeof(char) * (length - n + 1));
+		if (!stash)
+			return (NULL);
+		stash[length - n + 1] = '\0';
+		stash = gnl_cpy(stash, &src[n + 1], length - n);
+	}
+	return (stash);
+}
+
+static char	*gnl_cpy(char *dst, char *src, int end)
 {
 	int	i;
 
@@ -93,7 +94,7 @@ static char	*gnl_cpy(char *dst, char *src, int start, int end)
 	i = 0;
 	while (i < end)
 	{
-		dst[start + i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
 	return (dst);
